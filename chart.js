70abe00bd5738ae42702b4481121bbe694798072
zakiibnu723 <123972239+zakiibnu723@users.noreply.github.com>
2023-12-14 05:11:09 +0700

    Add files via upload

---
+++ chart.js
@@ -0,0 +1,257 @@
+
+function generateChart(suhu, tanggal) {
+    // Hapus chart lama jika ada
+
+
+
+    const labels = [
+        '00.00', '01.00', '02.00', '03.00', 
+        '04.00', '05.00', '06.00', '07.00', 
+        '08.00', '09.00', '10.00', '11.00', 
+        '12.00', '13.00', '14.00', '15.00', 
+        '16.00', '17.00', '18.00', '19.00', 
+        '20.00', '21.00', '22.00', '23.00', 
+    ]
+    const ctx = myChart.getContext('2d');
+    chart = new Chart(ctx, {
+        scaleFontColor: '#ffffff',
+        type: 'line',
+        data: {
+            labels: labels,
+            datasets: [{
+                label: `${tanggal[0]} | temperature: `,
+                data: suhu[0],
+                fill: true,
+                color: '#ffffff',
+                backgroundColor: 'rgba(0, 234, 242, 0.157)',
+                borderColor: '#FFEA00',
+                lineTension: 0.6,
+                borderWidth: 1.5,
+                order: 2,
+
+            }, {
+                label: `${tanggal[1]} | temperature: `,
+                data: suhu[1],
+                fill: true,
+                color: '#ffffff',
+                backgroundColor: 'rgba(138, 138, 138, 0.035)',
+                borderColor: 'rgba(105, 105, 105, 0.295)',
+                lineTension: 0.1,
+                borderWidth: 1.5,
+                hidden: true,
+                order: 0
+
+            },{
+                label: `${tanggal[2]} | temperature: `,
+                data: suhu[2],
+                fill: true,
+                color: '#ffffff',
+                backgroundColor: 'rgba(138, 138, 138, 0.035)',
+                borderColor: 'rgba(105, 105, 105, 0.295)',
+                lineTension: 0.1,
+                borderWidth: 1.5,
+                hidden: true,
+                order: 0
+
+            },{
+                label: `${tanggal[3]} | temperature: `,
+                data: suhu[3],
+                fill: true,
+                color: '#ffffff',
+                backgroundColor: 'rgba(138, 138, 138, 0.035)',
+                borderColor: 'rgba(105, 105, 105, 0.295)',
+                lineTension: 0.1,
+                borderWidth: 1.5,
+                hidden: true,
+                order: 0
+
+            },{
+                label: `${tanggal[4]} | temperature: `,
+                data: suhu[4],
+                fill: true,
+                color: '#ffffff',
+                backgroundColor: 'rgba(138, 138, 138, 0.035)',
+                borderColor: 'rgba(105, 105, 105, 0.295)',
+                lineTension: 0.1,
+                borderWidth: 1.5,
+                hidden: true,
+                order: 0
+
+            },{
+                label: `${tanggal[5]} | temperature: `,
+                data: suhu[5],
+                fill: true,
+                color: '#ffffff',
+                backgroundColor: 'rgba(138, 138, 138, 0.035)',
+                borderColor: 'rgba(105, 105, 105, 0.295)',
+                lineTension: 0.1,
+                borderWidth: 1.5,
+                hidden: true,
+                order: 0
+
+            },{
+                label: `${tanggal[6]} | temperature: `,
+                data: suhu[6],
+                fill: true,
+                color: '#ffffff',
+                backgroundColor: 'rgba(138, 138, 138, 0.035)',
+                borderColor: 'rgba(105, 105, 105, 0.295)',
+                lineTension: 0.1,
+                borderWidth: 1.5,
+                hidden: true,
+                order: 0
+            }]
+        },
+        options: {
+            plugins: {
+                title: {
+                    display: false,
+                    text: 'Hourly Forecast in 5 days'
+                },
+
+                legend: {
+                    display: false
+                },
+                tooltip: {
+                    callbacks: {
+                        label: function (context) {
+                            let label = context.dataset.label || '';
+                            if (context.parsed.y !== null) {
+                                label += context.parsed.y + ' °C';
+                            }
+                            return label;
+                        },
+
+                    },
+                },
+            },
+            scales: {
+                x: {
+                    title: {
+                        display: false,
+                        text: 'Hour'
+                    },
+                    ticks: {
+                        display: true,
+                        color: '#ffffff',
+                        maxTicksLimit: 8,
+                        stepSize: 8
+                    },
+                    grid : {
+                        color : '#70707053',
+                        lineWidth: 0.5
+                    }
+                },
+                y: {
+                    title: {
+                        display: false,
+                        text: 'Temperature'
+                    },
+
+                    ticks: {
+                        display: true,
+                        color: '#ffffff',
+                        stepSize: 4,
+                        beginAtZero: true
+                    },
+                    grid : {
+                        color : '#70707053',
+                        lineWidth: 0.5
+
+                    }
+                },
+                
+            },
+            elements: {
+                point:{
+                    radius: 2,
+                    hoverRadius: 7,
+
+                }
+            },
+            tooltips: {
+                mode: 'index',
+                intersect: false,
+            },
+            
+            onClick: function(event, elements) {
+                if (elements && elements.length > 0) {
+                    const firstElement = elements[0];
+
+                    DisplayDataHour(firstElement.datasetIndex, firstElement.index);
+                }
+            },
+            
+            aspectRatio : false,
+            responsive: true,
+            maintainAspectRatio: false,
+            width: 3200,
+            height: 250,
+            chartArea: {
+            backgroundColor: 'rgba(251, 85, 85, 0.4)'
+    }
+        },
+        
+    });
+    
+    // chart.style.backgroundColor = '#6060600f';
+    
+    
+    // chart.data.datasets[2].hidden = false;
+}
+
+function DisplayDataHour(indexhari, indexjam) {
+    const target = listAllDataHours[indexhari][indexjam];
+    console.log(target)
+    target.onDisplay();
+    updateTheme(target.sunriseTime, target.currentTime, target.sunsetTime, target.icon)
+}
+
+function clearData(chart) {
+    chart.data.datasets.forEach((dataset) => {
+        dataset.data = [];
+    });
+    chart.update();
+}
+
+function addDataArray(chart, newData) {
+    chart.data.datasets.forEach((dataset, index) => {
+        dataset.data.push(...newData[index]);
+    });
+    chart.update();
+}
+
+function displayChart(index, hiddenStatus) {
+    chart.data.datasets[index].hidden = hiddenStatus
+    console.log(hiddenStatus)
+    chart.update()
+        chart.render({
+            duration: 0,  // Atur durasi animasi dalam milidetik
+            easing: 'easeInOutQuad',  // Atur jenis animasi (opsional)
+        })
+
+}   
+
+
+
+function addHover(evt, index, legend) {
+    const dataset = legend.chart.data.datasets[index];
+
+    // Tambahkan efek hover untuk dataset tertentu
+    dataset.borderColor = '#FFEA00';
+    dataset.backgroundColor = 'rgba(96, 96, 96, 0.456)';
+    dataset.order = 99;
+
+    legend.chart.update();
+}
+  
+function removeHover(evt, index, legend) {
+    const dataset = legend.chart.data.datasets[index];
+  
+    // Kembalikan warna ke nilai default
+    dataset.borderColor = 'rgba(105, 105, 105, 0.250)';
+    dataset.backgroundColor = 'rgba(138, 138, 138, 0.060)';
+    dataset.order = 0;
+
+    legend.chart.update();
+}
---
+++ main.js
@@ -0,0 +1,236 @@
+// document.addEventListener('DOMContentLoaded', function() {
+//     setTimeout(function() {
+//         document.querySelector('.preloader-container').style.display = 'none';
+//     }, 4000)
+// })
+
+const preloader = document.querySelector('.preloader-container');
+const backgroundLoader = document.querySelector('.background-loader')
+
+window.onload = function() {
+    setTimeout(function() {
+        preloader.style.opacity = 0;
+    }, 800)
+}
+
+
+backgroundVideo.addEventListener('loadstart', function() {
+    backgroundLoader.style.opacity = 1;
+    
+})
+backgroundVideo.addEventListener('canplaythrough', function() {
+    // setTimeout(function() {
+    backgroundLoader.style.opacity = 0;
+    // }, 100)
+})
+
+
+// backgroundImage.addEventListener('load', function() {
+//     preloader.style.opacity = 1;
+// })
+
+backgroundImage.onload = function() {
+    setTimeout(function() {
+        backgroundLoader.style.opacity = 0;
+    }, 100)
+}
+
+
+
+
+
+const defaultWilayah = "yogyakarta";
+
+
+class DataSet {
+    constructor (
+        wilayah, timestamp, timezone, icon, suhu, cuaca, 
+        kelembapan, angin, tekanan, sunrisetime, sunsettime
+    ) {
+        this.wilayah = wilayah;
+        this.timestamp = timestamp;
+        this.timezone = timezone;
+        this.cuaca = cuaca;
+        this.suhu = Math.round(suhu);
+        this.icon = icon;
+        this.kelembapan = kelembapan;
+        this.angin = angin;
+        this.tekanan = tekanan;
+        this.sunriseTime = sunrisetime;
+        this.sunsetTime = sunsettime;
+    }
+
+    onDisplay() {   
+        document.getElementById('icon-cuaca').src = `asset/weather-icon/${this.icon}.svg` ;
+        document.getElementById('suhu').innerHTML = this.suhu;
+        document.getElementById('cuaca').innerHTML = this.cuaca;
+        document.getElementById('humidity').innerHTML = this.kelembapan + "%";
+        document.getElementById('wind').innerHTML = `${this.angin} km/h`;
+        document.getElementById('pressure').innerHTML = `${this.tekanan} hPa`;
+        document.getElementById('wilayah').innerHTML = `${this.wilayah}`;
+        document.getElementById('time').innerHTML = this.currentTime;
+        document.getElementById('date').innerHTML = this.longDate;
+        console.log("wilayah: " + this.wilayah)
+        console.log("condition: " + this.cuaca);
+        console.log("weahter icon: " + this.icon)
+        console.log("sunrise: " + this.sunriseTime);
+        console.log("sunset: " + this.sunsetTime);
+    }
+
+    dateTimeLocalFormat(option) {
+        const UTCtime = new Date(this.timestamp * 1000)
+        const localTime = UTCtime.toLocaleString('id-ID', {
+            timeZone: this.timezone, ...option
+        })
+
+        return localTime;
+    }
+
+
+    get currentTime() {
+        return this.dateTimeLocalFormat({hour: '2-digit', minute: '2-digit'});
+    }
+
+    get longDate() {
+        return this.dateTimeLocalFormat({day: 'numeric', month: 'long', year: 'numeric'});
+    }
+
+    get shortDate() {
+        return this.dateTimeLocalFormat({day: 'numeric', month: 'short'});
+    }
+}
+
+
+
+const search = document.querySelector('.search');
+const searchbar = document.querySelector('.search input');
+const myChart = document.getElementById('myChart');
+let chart;
+let listAllDataHours;
+let forecastDesc;
+
+//get weather data object from API 
+async function checkWeather(wilayah) {
+    
+    const API_Url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${wilayah}/next7days?unitGroup=metric&key=TY6AA7XBHB3Z6PHMYUEKRBH6T&iconSet=icons2`;
+    const response = await fetch(API_Url);
+    const data = await response.json();
+    // console.log(data);
+
+    const currentData = new DataSet (
+        data.resolvedAddress, 
+        data.currentConditions.datetimeEpoch,
+        data.timezone, 
+        data.currentConditions.icon,
+        data.currentConditions.temp, 
+        data.currentConditions.conditions,
+        data.currentConditions.humidity, 
+        data.currentConditions.windspeed, 
+        data.currentConditions.pressure,
+        data.currentConditions.sunrise,
+        data.currentConditions.sunset
+        );
+    currentData.onDisplay();
+
+    console.log(currentData);
+        
+    listAllDataHours = [];
+    forecastDesc = [];
+    let tempList = [];
+    let tanggal = [];
+        
+    for (let i = 0; i < 7; i++) {
+        let hourObjectPerDay = [];
+        let tempPerDay = [];
+        for (let j = 0; j < 24; j++) {
+            const dataHour = new DataSet (
+                data.resolvedAddress,
+                data.days[i].hours[j].datetimeEpoch,
+                data.timezone, 
+                data.days[i].hours[j].icon,
+                data.days[i].hours[j].temp, 
+                data.days[i].hours[j].conditions,
+                data.days[i].hours[j].humidity, 
+                data.days[i].hours[j].windspeed, 
+                data.days[i].hours[j].pressure,
+                data.days[i].sunrise,
+                data.days[i].sunset
+            )
+            // listHourObjects[i].push(dataHour);
+            hourObjectPerDay.push(dataHour);
+            tempPerDay.push(dataHour.suhu);
+            
+        }
+
+        listAllDataHours.push(hourObjectPerDay);
+        forecastDesc.push(data.days[i].description);
+        tempList.push(tempPerDay);
+        tanggal.push(hourObjectPerDay[0].shortDate);
+
+        
+        updateDaycard(i, tanggal[i], data.days[i].icon, data.days[i].tempmin, data.days[i].tempmax )
+    }
+    
+    updateTheme(currentData.sunriseTime, currentData.currentTime, currentData.sunsetTime, currentData.icon);  
+    
+    if (chart) {
+        // Jika chart sudah ada, hapus data lama dan tambahkan data baru
+        clearData(chart);
+        addDataArray(chart, tempList, tanggal);
+    } else {
+        // Jika chart belum ada, buat chart baru
+        generateChart(tempList, tanggal);
+    }
+    searchbar.value = '';
+    
+    resetAllDayCards();
+    setOneDayCard(0);
+    // listAllDataHours[0][0].onDisplay();
+}
+
+// getListTime(data2);
+checkWeather(defaultWilayah);
+
+search.addEventListener('submit', function(event) {
+    event.preventDefault();
+    checkWeather(searchbar.value);
+})
+
+
+
+const dayCards = document.querySelectorAll('.daycard');
+function updateDaycard(index, tanggal, namaIcon, suhuMin, suhuMax){
+    document.querySelector(`#daycard${index} .tanggal`).innerHTML = tanggal;
+    document.querySelector(`#daycard${index} .icon img`).src = `asset/weather-icon/${namaIcon}.svg`;
+    document.querySelector(`#daycard${index} .temp-min`).innerHTML = Math.round(suhuMin) + '°';
+    document.querySelector(`#daycard${index} .temp-max`).innerHTML = Math.round(suhuMax) + '°';   
+    // document.querySelector('.forecast-desc .desc-text').innerHTML = forecastDesc[0];
+    // resetAllDaycards();
+    // setOneDaycard(0);
+}
+
+
+dayCards.forEach((dayCard, index) => { 
+    dayCard.addEventListener('click', function() {
+        resetAllDayCards();
+        setOneDayCard(index); // Pass the index directly
+    });
+});
+
+function resetAllDayCards() {
+    dayCards.forEach((dayCard, index) => {
+        //remove class clicked in daycard and set all hidden status true.
+        dayCard.classList.remove('clicked');
+        //hidden all chart datasets
+        displayChart(index, true);
+        removeHover(null, index, { chart: chart });
+    })
+}
+
+function setOneDayCard(index) {
+    //set clicked daycard hidden-status to false (ondisplay)
+    displayChart(index, false);
+    addHover(null, index, { chart: chart }); // Simulate hover
+    dayCards[index].classList.add('clicked'); // Use index to access the correct dayCard
+    document.querySelector('.forecast-desc .desc-text').innerHTML = forecastDesc[index];
+}
